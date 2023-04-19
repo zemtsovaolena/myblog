195b82b1d6c9674366de353242cf1d6ea0d727c3
Olena <sale.gorka@gmail.com>
2023-04-19 20:33:50 +0200

    Add Procfile gitignor2 delete port 8080

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 import os
-import email_validator
 
 
 app = Flask(__name__)
